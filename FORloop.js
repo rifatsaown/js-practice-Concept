bfcf259276d447cf0602a952b913c62529d27d5c
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2022-12-07 22:33:33 +0600

    for loop explore

---
+++ FORloop.js
@@ -0,0 +1,24 @@
+let numbers = {
+    rifat: 95,
+    rumi: 99,
+    saown: 89,
+    ratri: 94,
+}
+console.log("------------------------------");
+// Normal for loop
+for (let i = 0; i < Object.keys(numbers).length; i++) {
+    console.log(`Name: ${Object.keys(numbers)[i]} \nMarks: ${numbers[Object.keys(numbers)[i]]}\n`);
+}
+console.log("------------------------------");
+
+// for in loop
+for(let number in numbers){
+    console.log(`Name: ${number} Marks: ${numbers[number]}\n`);
+}
+console.log("------------------------------");
+// for of loop
+let names = ["Rifat", "Rumi", "Saown", "Ratri"];
+for(let name of names){
+    console.log(name);
+}
+console.log("------------------------------");
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+// for of loop
+// let names = ["Rifat", "Rumi", "Saown", "Ratri"];
+// for(let name of names){
+//     console.log(name);
+// }
