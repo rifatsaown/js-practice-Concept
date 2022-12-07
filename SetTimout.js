073aa30c1e196772575b2c91ca3d4c049a557e1c
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2022-12-07 21:40:53 +0600

    set timeout founction

---
+++ SetTimout.js
@@ -0,0 +1,20 @@
+const task1 =()=> {
+    console.log('Task 1');
+}
+
+const task2 =()=> {
+    setTimeout(() => {
+        console.log('Task 2');
+    }, 0);
+}
+const task3 =()=> {
+    console.log('Task 3');
+}
+const task4 =()=> {
+    console.log('Task 4');
+}
+
+task1();
+task2();
+task3();
+task4();
