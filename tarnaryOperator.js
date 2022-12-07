c1894b15dba01ae7714563e6f83b9d68c688f8f9
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2022-12-07 21:48:59 +0600

    tarnary oerator

---
+++ tarnaryOperator.js
@@ -0,0 +1,14 @@
+//Ternary Operator example
+const age = 18;
+const message = age > 18 ? 'Welcome' : 'Sorry';
+console.log(message);
+
+//Ternary Operator example with multiple condition
+const age = 18;
+const message = age > 18 && age < 30 ? 'Welcome' : 'Sorry';
+console.log(message);
+
+//Ternary Operator example with multiple condition
+const age = 18;
+const message = age > 18 || age < 30 ? 'Welcome' : 'Sorry';
+console.log(message);
