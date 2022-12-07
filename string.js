02be8e544889a21d9faae60d78b0ce930f2fde24
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2022-12-07 23:23:58 +0600

    string

---
+++ string.js
@@ -0,0 +1,38 @@
+let name = "Md Rifat Hossen Saown";
+
+// String length
+console.log(name.length);
+console.log("------------------------------");
+
+// String to uppercase
+console.log(name.toUpperCase());
+console.log("------------------------------");
+
+// String to lowercase
+console.log(name.toLowerCase());
+console.log("------------------------------");
+
+// String Slice
+console.log(name.slice(2,8));
+console.log(name.slice(2));
+console.log("------------------------------");
+
+// String to array
+console.log(name.split(""));
+console.log("------------------------------");
+
+// String to array with separator
+console.log(name.split(" "));
+console.log("------------------------------");
+
+// String replace
+console.log(name.replace("Saown", "Rif"));
+console.log("------------------------------");
+
+// String concat
+console.log(name.concat(" is a web developer"));
+console.log("------------------------------");
+
+//String trim
+let name2 = "   Rifat   ";
+console.log(name2.trim());
--- tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-// for of loop
-// let names = ["Rifat", "Rumi", "Saown", "Ratri"];
-// for(let name of names){
-//     console.log(name);
-// }
