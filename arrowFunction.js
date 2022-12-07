47c8f8d1bff1f0090e86e2b831ad6d7a242a8c91
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2022-12-07 21:47:57 +0600

    arrow function

---
+++ arrowFunction.js
@@ -0,0 +1,17 @@
+//normal Function example
+function addab(a, b) {
+    return a + b;
+}
+console.log(addab(1, 2));
+
+//arrow function example
+const addNumber = (a, b) => {
+    return a + b;
+}
+console.log(addNumber(1, 2));
+
+//arrow function example without parameter
+const add = () => {
+    return 1 + 2;
+}
+console.log(add());
