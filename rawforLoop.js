4d7c73f11a557b2c6c1981499177d240695c093d
Md Rifat Hossen <rifatsaown0@gmail.com>
2022-12-11 08:16:49 +0600

    time compleaxity mesure

--- rawforLoop.js
+++ rawforLoop.js
@@ -1,3 +1,5 @@
 for(let i = 0; i < 10000; i++){
     console.log(i);
 }
+//This code give time complexity
+// O(1) = Constant  // O(n) = Linear // O(n^2) = Quadratic // O(log n) = Logarithmic    // O(2^n) = Exponential // O(n!) = Factorial    // O(n^3) = Cubic  
