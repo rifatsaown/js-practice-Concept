604cdb939516aa02345aec42273d0d22c2aa7efb
Md Rifat Hossen <rifatsaown0@gmail.com>
2022-12-12 08:18:16 +0600

    variable name change

--- tarnaryOperator.js
+++ tarnaryOperator.js
@@ -4,11 +4,11 @@ const message = age > 18 ? 'Welcome' : 'Sorry';
 console.log(message);
 
 //Ternary Operator example with multiple condition
-const age = 18;
-const message = age > 18 && age < 30 ? 'Welcome' : 'Sorry';
-console.log(message);
+const age2 = 18;
+const message2 = age > 18 && age < 30 ? 'Welcome' : 'Sorry';
+console.log(message2);
 
 //Ternary Operator example with multiple condition
-const age = 18;
-const message = age > 18 || age < 30 ? 'Welcome' : 'Sorry';
+const age3 = 18;
+const message3 = age > 18 || age < 30 ? 'Welcome' : 'Sorry';
 console.log(message);
