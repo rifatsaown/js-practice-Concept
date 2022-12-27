914fa1d394be55dcead90a5f0e418bc7f9c4cf2d
Md Rifat Hossen <rifatsaown0@gmail.com>
2022-12-27 08:34:16 +0600

    log ---------------------

--- allLoop.js
+++ allLoop.js
@@ -2,6 +2,7 @@
 for(let i = 0; i < 100; i++){
     console.log(i);
 }
+console.log("-------------------------");
 
 // while loop
 let i = 0;
@@ -9,6 +10,7 @@ while(i < 100){
     console.log(i);
     i++;
 }
+console.log("-------------------------");
 
 // do while loop
 let i2 = 0;
@@ -16,6 +18,7 @@ do {
     console.log(i2);
     i2++;
 } while(i2 < 100);
+console.log("-------------------------");
 
 // for...in loop
 let obj = {
@@ -25,18 +28,21 @@ let obj = {
 for(let key in obj){
     console.log(key);
 }
+console.log("-------------------------");
 
 // for...of loop
 let arr = [1,2,3,4,5];
 for(let value of arr){
     console.log(value);
 }
+console.log("-------------------------");
 
 // forEach loop
 let arr2 = [1,2,3,4,5];
 arr2.forEach((value, index) => {
     console.log(value, index);
 })
+console.log("-------------------------");
 
 // map loop
 let arr3 = [1,2,3,4,5];
@@ -45,6 +51,7 @@ let newArr3 = arr3.map((value, index) => {
     return value + 1;
 })
 console.log(newArr3);
+console.log("-------------------------");
 
 // filter loop
 let arr4 = [1,2,3,4,5];
@@ -53,6 +60,7 @@ let newArr4 = arr4.filter((value, index) => {
     return value > 2;
 })
 console.log(newArr4);
+console.log("-------------------------");
 
 // reduce loop
 let arr5 = [1,2,3,4,5];
@@ -60,6 +68,7 @@ let result = arr5.reduce((acc, value) => {
     return acc + value;
 }, 0)
 console.log(result);
+console.log("-------------------------");
 
 // every loop
 let arr6 = [1,2,3,4,5];
@@ -67,6 +76,7 @@ let result6 = arr6.every((value) => {
     return value > 2;
 })
 console.log(result6);
+console.log("-------------------------");
 
 // some loop
 let arr7 = [1,2,3,4,5];
@@ -74,6 +84,7 @@ let result7 = arr7.some((value) => {
     return value > 2;
 })
 console.log(result7);
+console.log("-------------------------");
 
 // find loop
 let arr8 = [1,2,3,4,5];
@@ -81,6 +92,7 @@ let result8 = arr8.find((value) => {
     return value > 2;
 })
 console.log(result8);
+console.log("-------------------------");
 
 // findIndex loop
 let arr9 = [1,2,3,4,5];
@@ -88,6 +100,7 @@ let result9 = arr9.findIndex((value) => {
     return value > 2;
 })
 console.log(result9);
+console.log("-------------------------");
 
 // for...in loop
 let obj01 = {
@@ -97,3 +110,4 @@ let obj01 = {
 for(let key in obj01){
     console.log(key);
 }
+console.log("-------------------------");
