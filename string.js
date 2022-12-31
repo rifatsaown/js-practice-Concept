31aafe6b807eb6e56bee49599c76d369f4f39882
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-01-01 00:54:43 +0600

    js cheak

--- string.js
+++ string.js
@@ -2,36 +2,36 @@ let name = "Md Rifat Hossen Saown";
 
 // String length
 console.log(name.length);
-console.log("------------------------------");
+console.log("-----------------------------");
 
 // String to uppercase
 console.log(name.toUpperCase());
-console.log("------------------------------");
+console.log("-----------------------------");
 
 // String to lowercase
 console.log(name.toLowerCase());
-console.log("------------------------------");
+console.log("-----------------------------");
 
 // String Slice
 console.log(name.slice(2,8));
 console.log(name.slice(2));
-console.log("------------------------------");
+console.log("-----------------------------");
 
 // String to array
 console.log(name.split(""));
-console.log("------------------------------");
+console.log("-----------------------------");
 
 // String to array with separator
 console.log(name.split(" "));
-console.log("------------------------------");
+console.log("-----------------------------");
 
 // String replace
 console.log(name.replace("Saown", "Rif"));
-console.log("------------------------------");
+console.log("-----------------------------");
 
 // String concat
 console.log(name.concat(" is a web developer"));
-console.log("------------------------------");
+console.log("-----------------------------");
 
 //String trim
 let name2 = "   Rifat   ";
