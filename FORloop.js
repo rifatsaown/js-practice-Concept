3daa2193f4a60b1542c310e67aa59af187c767dc
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-01-04 00:56:19 +0600

    js cheak

--- FORloop.js
+++ FORloop.js
@@ -5,21 +5,21 @@ let numbers = {
     ratri: 94,
     indur: 98,
 }
-console.log("-----------------------------");
+console.log("----------------------------");
 // Normal for loop
 for (let i = 0; i < Object.keys(numbers).length; i++) {
     console.log(`Name: ${Object.keys(numbers)[i]} \nMarks: ${numbers[Object.keys(numbers)[i]]}\n`);
 }
-console.log("-----------------------------");
+console.log("----------------------------");
 
 // for in loop
 for (let number in numbers) {
     console.log(`Name: ${number} Marks: ${numbers[number]}\n`);
 }
-console.log("-----------------------------");
+console.log("----------------------------");
 // for of loop
 let names = ["Rifat", "Rumi", "Saown", "Ratri"];
 for (let name of names) {
     console.log(name);
 }
-console.log("-----------------------------");
+console.log("----------------------------");
